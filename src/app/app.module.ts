e63a5ecebfc644f991e4d3f51f427b419a4bc451
Mohammad Farooqi <mohammad.farooqi@gmail.com>
2018-01-21 22:34:06 -0500

    added routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { SignupComponent } from './client/activity/signup/signup.component';
+import { ListComponent } from './client/activity/list/list.component';
+import { HomeComponent } from './client/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    ListComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    RouterModule.forRoot([
+      {
+        path: '', component: HomeComponent
+      },
+      {
+        path: 'activity/signup', component: SignupComponent
+      },
+      {
+        path: 'activity/list', component: ListComponent
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
